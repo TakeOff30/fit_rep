0f77fffef2bbc612e73d292c4b7cb524e40e42c8
ela-rami <elarami4real@gmail.com>
2024-05-03 04:47:36 +0200

    Added bar chart for weekly kcal in stats page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,7 +7,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIcon = 3; // 3: weekly kcal selezionato di default
+  int _selectedIcon = 3; // Default selection for "Weekly kcal"
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +29,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
             ),
-            const SizedBox(height: 50), // Spazio tra il titolo e il riquadro informativo
+            const SizedBox(height: 50),
             Center(
               child: Container(
                 width: 300,
@@ -43,16 +44,20 @@ class _HomeScreenState extends State<HomeScreen> {
                     Expanded(
                       child: Row(
                         children: <Widget>[
-                          _buildIconSection(Icons.emoji_events, '3', true, true),
-                          _buildIconSection(Icons.fitness_center, '0/3', false, true),
+                          _buildIconSection(
+                              Icons.emoji_events, '3', true, true),
+                          _buildIconSection(
+                              Icons.fitness_center, '0/3', false, true),
                         ],
                       ),
                     ),
                     Expanded(
                       child: Row(
                         children: <Widget>[
-                          _buildIconSection(Icons.local_fire_department, 'XXX KCAL', true, false),
-                          _buildIconSection(Icons.timer, 'XXX MIN', false, false),
+                          _buildIconSection(Icons.local_fire_department,
+                              'XXX KCAL', true, false),
+                          _buildIconSection(
+                              Icons.timer, 'XXX MIN', false, false),
                         ],
                       ),
                     ),
@@ -60,15 +65,95 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
             ),
-            const SizedBox(height: 80), // Spazio tra il riquadro informativo e le icone interattive
-            _buildIconRow(), // Costruisce la fila delle icone interattive
+            const SizedBox(height: 80),
+            _buildIconRow(),
+            if (_selectedIcon ==
+              3) // Display bar chart if "Weekly kcal" is selected
+              Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 0),
+                child: BarChart(
+                BarChartData(
+                  barGroups: [
+                  BarChartGroupData(x: 0, barRods: [
+                    BarChartRodData(
+                      toY: 12, color: Colors.lightBlue, width: 16)
+                  ]),
+                  BarChartGroupData(x: 1, barRods: [
+                    BarChartRodData(
+                      toY: 16, color: Colors.lightBlue, width: 16)
+                  ]),
+                  BarChartGroupData(x: 2, barRods: [
+                    BarChartRodData(
+                      toY: 0, color: Colors.lightBlue, width: 16)
+                  ]),
+                  BarChartGroupData(x: 3, barRods: [
+                    BarChartRodData(
+                      toY: 18, color: Colors.lightBlue, width: 16)
+                  ]),
+                  BarChartGroupData(x: 4, barRods: [
+                    BarChartRodData(
+                      toY: 20, color: Colors.lightBlue, width: 16)
+                  ]),
+                  BarChartGroupData(x: 5, barRods: [
+                    BarChartRodData(
+                      toY: 0, color: Colors.lightBlue, width: 16)
+                  ]),
+                  BarChartGroupData(x: 6, barRods: [
+                    BarChartRodData(
+                      toY: 14, color: Colors.lightBlue, width: 16)
+                  ]),
+                  ],
+                  gridData: FlGridData(show: false),
+                  borderData: FlBorderData(show: false),
+                  titlesData: FlTitlesData(
+                  show: true,
+                  bottomTitles: AxisTitles(
+                    sideTitles: SideTitles(
+                    showTitles: true,
+                    reservedSize: 40,
+                    getTitlesWidget: (value, meta) {
+                      final titles = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
+                      return Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Text(titles[value.toInt() % titles.length],
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14)),
+                      );
+                    },
+                    interval: 1,
+                    ),
+                  ),
+                  ),
+                  groupsSpace:
+                    30, // Aumentato lo spazio tra i gruppi di barre
+                  alignment: BarChartAlignment
+                    .center, // Modificato per utilizzare l'allineamento che permette l'uso di groupsSpace
+                  barTouchData: BarTouchData(
+                  enabled: true,
+                  touchTooltipData: BarTouchTooltipData(
+                    getTooltipItem: (group, groupIndex, rod, rodIndex) {
+                    return BarTooltipItem(
+                      rod.toY.toString() + ' kcal',
+                      TextStyle(color: Colors.white),
+                    );
+                    },
+                    getTooltipColor: (_) => Colors.blueGrey,
+                    tooltipBorder: BorderSide(color: Colors.white),
+                  ),
+                  ),
+                ),
+                ),
+              ),
+              ),
           ],
         ),
       ),
     );
   }
 
-  // Fila delle icone interattive per grafici
   Widget _buildIconRow() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -80,7 +165,6 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  // Icone grafici interattive
   Widget _iconButton(int id, IconData icon, String label) {
     bool isSelected = _selectedIcon == id;
     return Column(
@@ -108,14 +192,18 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  // Icone e testo del riquadro informativo
-  Widget _buildIconSection(IconData icon, String label, bool borderRight, bool borderBottom) {
+  Widget _buildIconSection(
+      IconData icon, String label, bool borderRight, bool borderBottom) {
     return Expanded(
       child: Container(
         decoration: BoxDecoration(
           border: Border(
-            right: borderRight ? const BorderSide(width: 2, color: Color(0xFFC0C0C0)) : BorderSide.none,
-            bottom: borderBottom ? const BorderSide(width: 2, color: Color(0xFFC0C0C0)) : BorderSide.none,
+            right: borderRight
+                ? const BorderSide(width: 2, color: Color(0xFFC0C0C0))
+                : BorderSide.none,
+            bottom: borderBottom
+                ? const BorderSide(width: 2, color: Color(0xFFC0C0C0))
+                : BorderSide.none,
           ),
         ),
         child: Center(
@@ -124,7 +212,12 @@ class _HomeScreenState extends State<HomeScreen> {
             children: <Widget>[
               Icon(icon, size: 32, color: const Color(0xFF39FF14)),
               const SizedBox(height: 8),
-              Text(label, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white, fontFamily: 'Roboto')),
+              Text(label,
+                  style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontFamily: 'Roboto')),
             ],
           ),
         ),
